e0175a3a33582ed78da8ec1520b34c7d06254f9c
tomofumi-k <t.k.20202210@gmail.com>
2022-08-27 16:58:07 +0900

    Delete web-anim-liblary-test directory

--- web-anim-liblary-test/js/navColorChange.js
+++
@@ -1,9 +0,0 @@
-const navColorChange = document.getElementById("nav");
-window.addEventListener("scroll", function () {
-  const windowHeight = window.pageYOffset || document.documentElement.windowHeight;
-  if (windowHeight <= 40) {
-    navColorChange.style.background = "transparent";
-  } else if (windowHeight <= 800) {
-    navColorChange.style.background = "#0f0f0f";
-  }
-});
